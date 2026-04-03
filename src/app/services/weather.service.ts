f96935101d9abb16ee6fe42bbaf27e12d2f6b862
Sanjeev Merugu <sanjeevmerugu28@gmail.com>
2026-04-03 21:07:28 +0530

    adding weather service file

---
+++ src/app/services/weather.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  constructor(private http: HttpClient) { }
+
+  private API_KEY ="89e6aeb69e054689b3f165932263003";
+
+  private url ="https://api.weatherapi.com/v1";
+
+  getCurrentWeather(city: string){
+
+    const params = new HttpParams()
+    .set('key',this.API_KEY)
+    .set('q',city)
+
+    return this.http.get(`${this.url}/current.json${this.API_KEY}`,{params});
+
+  }
+}
