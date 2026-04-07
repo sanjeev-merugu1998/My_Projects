1b728237157033f5007b98338dc180da8abf60f4
Sanjeev Merugu <sanjeevmerugu28@gmail.com>
2026-04-07 10:21:20 +0530

    inject service in component file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,43 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MATERIAL_MODULES } from './shared/material-imports';
+import { WeatherService } from './services/weather.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,MATERIAL_MODULES],
+  imports: [RouterOutlet,FormsModule,CommonModule,MATERIAL_MODULES],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Weather_app';
+
+  weatherData: any;
+
+  city ='';
+  errorMsg ='';
+  loading: boolean= false;
+
+  constructor(private weatherApi:WeatherService){}
+
+  fetchWeather(){
+
+    if(!this.city.trim()) return;
+    this.loading = true;
+    this.errorMsg= '';
+    this.weatherApi.getCurrentWeather(this.city).subscribe({
+      next: (data) => {
+        this.weatherData = data;
+        this.loading = false;
+
+      },
+      error: () => {
+        this.errorMsg = 'City not found or API Error'
+        this.loading = true;
+      }
+    });
+  }
 }
