4a6e2e01a2247ed2b44b8d6ae204a6bd0217ad83
Sanjeev Merugu <sanjeevmerugu28@gmail.com>
2026-04-12 18:39:43 +0530

    Add localstorage and logic

--- src/app/services/transaction.service.ts
+++ src/app/services/transaction.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class TransactionService {
+export class TransactionService{
 
-  constructor() { }
+    
+
+  constructor() { 
+    const saved = localStorage.getItem('transactions');
+    if(saved){
+      this.transactions = JSON.parse(saved);
+      this.transactionSubject.next([...this.transactions]);
+    }
+  }
+
+  private transactionSubject = new BehaviorSubject<any[]>([]);
+  transaction$ = this.transactionSubject.asObservable();
+
+  private transactions: any[] =[];
+
+  addTransaction(transaction: any){
+    this.transactions.push(transaction);
+    this.transactionSubject.next([...this.transactions]);
+    this.updateState();
+  }
+
+  deleteTransaction(id: number) {
+  this.transactions = this.transactions.filter(tx => tx.id !== id);
+  this.transactionSubject.next([...this.transactions]);
+  this.updateState();
+}
+
+  getTransaction(){
+    return this.transactionSubject.value;
+  }
+
+  private updateState(){
+    localStorage.setItem('transactions',JSON.stringify(this.transactions));
+  }
+
 }
