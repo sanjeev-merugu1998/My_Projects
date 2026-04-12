3d0498bb959cab24369be6c46696be519be76921
Sanjeev Merugu <sanjeevmerugu28@gmail.com>
2026-04-12 18:54:08 +0530

    Adding Transaction_module

---
+++ src/app/shared/transaction-imports.ts
@@ -0,0 +1,10 @@
+import { TransactionListComponent } from "../components/transaction-list/transaction-list.component";
+import { TransactionFormComponent } from "../components/transaction-form/transaction-form.component";
+import { DashboardComponent } from "../components/dashboard/dashboard.component";
+
+export const TRANSACTION_MODULE =[
+    TransactionListComponent,
+    TransactionFormComponent,
+    DashboardComponent
+
+]
