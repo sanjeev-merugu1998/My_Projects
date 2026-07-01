6eac1971ea1b3837a8fef41eb5fd29306c572b9f
Sanjeev Merugu <sanjeevmerugu28@gmail.com>
2026-07-01 16:38:06 +0530

    adding filter code

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-filter',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './filter.component.html',
   styleUrl: './filter.component.css'
 })
 export class FilterComponent {
+
+  @Output() filterChanged = new EventEmitter<any>();
+
+  constructor(private fb: FormBuilder){}
+
+  categories: string[] = ['Food','Travel','Rent','Salary','Shopping','Bills'];
+
+  filterForm = this.fb.group({
+    type:[''],
+    category:[''],
+    startDate:[''],
+    endDate:['']
+  });
+
+  applyFilter(){
+    this.filterChanged.emit(this.filterForm.value);
+  }
+
+  resetFilter(){
+    this.filterForm.reset({
+      type:'',
+      category:'',
+      startDate:'',
+      endDate:''
+    });
+
+    this.filterChanged.emit(this.filterForm.value);
+  }
 
 }
