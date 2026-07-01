f1483c9d1ee65f67709f6f4276c1cbf509747e0b
Sanjeev Merugu <sanjeevmerugu28@gmail.com>
2026-07-01 16:36:04 +0530

    integrating Chartjs

--- src/app/components/charts/charts.component.ts
+++ src/app/components/charts/charts.component.ts
@@ -1,12 +1,63 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component, Input, OnChanges, SimpleChanges, ViewChild,ElementRef } from '@angular/core';
+import { Chart,registerables } from 'chart.js';
+
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-charts',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './charts.component.html',
   styleUrl: './charts.component.css'
 })
-export class ChartsComponent {
+export class ChartsComponent implements OnChanges, AfterViewInit {
+
+  @Input() totalIncome: number = 0;
+  @Input() totalExpense: number =0;
+
+  @ViewChild('chartCanvas') chartCanvas!: ElementRef<HTMLCanvasElement>
+
+  chart: any;
+  viewInitialized = false;
+
+  ngAfterViewInit(): void {
+    this.viewInitialized = true;
+    this.createChart();
+  }
+  
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if(this.viewInitialized){
+    this.createChart();
+    }
+  }
+
+  createChart() {
+
+    // const canvas = document.getElementById('myChart') as HTMLCanvasElement;
+
+    if(!this.chartCanvas) return;
+
+    if (this.chart) {
+      this.chart.destroy();
+    }
+
+    this.chart = new Chart(this.chartCanvas.nativeElement, {
+      type: 'doughnut',
+      data: {
+        labels: ['Income', 'Expense'],
+        datasets: [
+          {
+            data: [this.totalIncome, this.totalExpense],
+            backgroundColor: ['#4CAF50', '#F44336']
+          }
+        ]
+      },
+      options: {
+        responsive: true
+      }
+    });
+  }
 
 }
